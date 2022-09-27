d0b06be3c88a64d10655446ba1ecd6840a59c445
Andrelise28 <andrelisebuttner@hotmail.com>
2022-09-27 20:17:39 -0300

    Iniciando projeto de animação com o Three.js

--- terra/Readme.md
+++ terra/Readme.md
@@ -1,9 +1,20 @@
- npm init
+
+🚀 Começando
+npm init
+npm start
+
+## Necessário instalar express e o three.js:
 
 npm install express
 
 npm install three@0.127.0
 
+```sh
+comandos para rodar a aplicação
+cd 
+terra
+npm init
+npm install express
+npm install three@0.127.0
 npm start
-
-# Earth
+```
---
+++ terra/public/index.js
@@ -0,0 +1,96 @@
+import * as THREE from '/build/three.module.js';
+import { OrbitControls } from '/jsm/controls/OrbitControls.js';
+
+let cena;
+let camera;
+let renderer;
+const canvas = document.querySelector('.fundo');
+
+cena = new THREE.Scene();
+
+const fov = 60;
+const aspecto = window.innerWidth / window.innerHeight;
+const proximidade = 0.1;
+const distancia = 1000;
+
+camera = new THREE.PerspectiveCamera(fov, aspecto, proximidade, distancia);
+camera.position.z = 2;
+cena.add(camera);
+
+renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+renderer.autoClear = false;
+renderer.setClearColor(0x000000, 0.0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const terraGeometry = new THREE.SphereGeometry(0.6, 32, 32);
+
+const terraMaterial = new THREE.MeshPhongMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: THREE.ImageUtils.loadTexture('texture/mapa2.jpg'),
+    bumpMap: THREE.ImageUtils.loadTexture('texture/specularmap.jpg'),
+    bumpScale: 0.3
+});
+
+const terraMesh = new THREE.Mesh(terraGeometry, terraMaterial);
+cena.add(terraMesh);
+
+const nuvemGeometry = new THREE.SphereGeometry(0.63, 32, 32);
+
+const nuvemMaterial = new THREE.MeshPhongMaterial({
+    map: THREE.ImageUtils.loadTexture('texture/nuvemTerra.png'),
+    transparent: true,
+});
+
+const nuvemMesh = new THREE.Mesh(nuvemGeometry, nuvemMaterial);
+cena.add(nuvemMesh);
+
+const estrelaGeometry = new THREE.SphereGeometry(80, 64, 64);
+
+const estrelaMaterial = new THREE.MeshBasicMaterial({
+    map : THREE.ImageUtils.loadTexture('texture/galaxy.png'),
+    side: THREE.BackSide
+});
+
+const estrelaMesh = new THREE.Mesh(estrelaGeometry, estrelaMaterial);
+cena.add(estrelaMesh);
+
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
+cena.add(ambientlight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.set(5, 3, 5);
+cena.add(pointLight);
+
+const Helper = new THREE.PointLightHelper(pointLight);
+cena.add(Helper);
+
+window.addEventListener('resize', () => {
+    camera.aspecto = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+}, false);
+
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    estrelaMesh.rotateX(0.002)
+    terraMesh.rotateX (0.0015);
+    nuvemMesh.rotateY (0.001);  
+    controls.update();
+    render();
+};
+
+const render = () => {
+    renderer.render(cena, camera);
+}
+
+animate();
